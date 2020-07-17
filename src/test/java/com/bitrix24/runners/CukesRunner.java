a358490db7101aa1973192e46c34383599f6f719
dildora-ahmed <dildora4@gmail.com>
2020-07-16 23:54:28 -0500

    runners

--- src/test/java/com/bitrix24/runners/CukesRunner.java
+++ src/test/java/com/bitrix24/runners/CukesRunner.java
@@ -1,4 +1,17 @@
 package com.bitrix24.runners;
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin={"html:target/cucumber-report.html",
+                "json:target/cucumber.json",
+                "rerun:target/rerun.txt"},
+        features="src/test/resources/features",
+        glue="com/bitrix24/step_definitions",
+        dryRun=false
+)
 public class CukesRunner {
 }
